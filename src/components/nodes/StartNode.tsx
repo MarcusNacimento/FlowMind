231f611e95b848796404ba2096a61557fbd1c40c
Marcus Vinicius <marcusvinicius2090@gmail.com>
2026-01-13 17:33:59 -0300

    fixing bug

--- src/components/nodes/StartNode.tsx
+++ src/components/nodes/StartNode.tsx
@@ -1,5 +1,9 @@
 export function StartNode(){
     return (
-        <div className= "px-4 py-2 bg-green-600 roudend text-sm font-medium"></div>
+        <div className= "px-4 py-2 bg-green-600 roudend text-sm font-medium">
+                  Start Node
+
+        </div>
+        
     )
 }
